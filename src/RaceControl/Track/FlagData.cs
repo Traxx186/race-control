e776f96f6eb670b0a6de834c50a932db23edd7d7
Traxx186 <justinvanderkruit@icloud.com>
2024-05-25 18:44:54 +0200

    refactor: only send driver number when set

--- src/RaceControl/Track/FlagData.cs
+++ src/RaceControl/Track/FlagData.cs
@@ -5,22 +5,31 @@ namespace RaceControl.Track;
 /// <summary>
 /// Object that contains a flag and the related driver.
 /// </summary>
-public class FlagData
+public class FlagData : ICloneable
 {
     /// <summary>
     /// The <see cref="Flag"/>
     /// </summary>
     [JsonConverter(typeof(JsonStringEnumConverter))]
-    public Flag Flag { get; init; }
+    public Flag Flag { get; set; }
 
     /// <summary>
     /// The driver number that related to the flag
     /// </summary>
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
-    public int Driver { get; init; }
-}
+    public int Driver { get; set; }
 
-public class FlagDataEventArgs : EventArgs
-{
-    public required FlagData FlagData { get; init; }
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    public object Clone()
+    {
+        var flagData = new FlagData()
+        {
+            Flag = Flag,
+            Driver = Driver
+        };
+
+        return flagData;
+    }
 }
