3826a124e046d577324962955da0e3a8aa9cbe4d
Traxx186 <justinvanderkruit@icloud.com>
2024-10-24 19:58:26 +0200

    feat: added sync sessions job

--- src/RaceControl/Jobs/SyncSessionsJob.cs
+++ src/RaceControl/Jobs/SyncSessionsJob.cs
@@ -4,17 +4,17 @@ using RaceControl.Database.Entities;
 
 namespace RaceControl.Jobs;
 
-public class SyncSessionsJob(RaceControlContext dbContext, ILogger<SyncSessionsJob> logger) : IJob
+public class SyncSessionsJob(RaceControlContext _dbContext, ILogger<SyncSessionsJob> _logger) : IJob
 {
-    private static readonly HttpClient HttpClient = new();
+    private static readonly HttpClient _httpClient = new();
 
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
     public async Task Execute(IJobExecutionContext context)
     {
-        logger.LogInformation("[Session Sync] Synchronizing session data with racing calendars");
-        var categories = dbContext.Categories.ToArray();
+        _logger.LogInformation("[Session Sync] Synchronizing session data with racing calendars");
+        var categories = _dbContext.Categories.ToArray();
         foreach (var category in categories)
         {
             // Fetch the calendar data of the current category, if no data is found go to the next
@@ -22,7 +22,7 @@ public class SyncSessionsJob(RaceControlContext dbContext, ILogger<SyncSessionsJ
             var calendar = await FetchCalendar(category.Key, DateTime.UtcNow.Year);
             if (null == calendar)
             {
-                logger.LogWarning("[Session Sync] Could not find session data for {key}", category.Key);
+                _logger.LogWarning("[Session Sync] Could not find session data for {key}", category.Key);
                 continue;
             }
 
@@ -35,53 +35,59 @@ public class SyncSessionsJob(RaceControlContext dbContext, ILogger<SyncSessionsJ
                 Time = s.Value
             }));
             
-            logger.LogInformation("[Session Sync] Update database sessions for {key}", category.Key);
+            _logger.LogInformation("[Session Sync] Update database sessions for {key}", category.Key);
             foreach (var race in races)
             {
                 // Query for a session with the given category, time and name
-                var existingSession = dbContext.Sessions
+                var existingSession = _dbContext.Sessions
                     .SingleOrDefault(s => s.CategoryKey == category.Key && s.Time == race.Time && s.Name == race.Name);
 
-                // If no session is found in the database, add the new session. Otherwise, the old session
+                // If no session is found in the database, add the new session. Otherwise the old session
                 // will be updated with the session time.
                 if (null == existingSession)
-                    dbContext.Sessions.Add(race);
+                    _dbContext.Sessions.Add(race);
                 else
                     existingSession.Time = race.Time;
             }
 
-            await dbContext.SaveChangesAsync();
+            await _dbContext.SaveChangesAsync();
         }
 
-        logger.LogInformation("[Session Sync] Session data synchronized");
+        _logger.LogInformation("[Session Sync] Session data synchronized");
     }
 
     /// <summary>
     /// Gets the sessions info for the given category
     /// </summary>
     /// <param name="category">The category to fetch the data for.</param>
-    /// <param name="year">The year of the season.</param>
     /// <returns>The fetched data.</returns>
     private async Task<Calendar?> FetchCalendar(string category, int year)
     {
-        logger.LogInformation("[Session Sync] Fetching calendar data for {key}", category);
+        _logger.LogInformation("[Session Sync] Fetching calendar data for {key}", category);
         
         var url = $"https://raw.githubusercontent.com/sportstimes/f1/main/_db/{category}/{year}.json";
-        return await HttpClient.GetFromJsonAsync<Calendar>(url);
+        return await _httpClient.GetFromJsonAsync<Calendar>(url);
     }
 
     /// <summary>
     /// The structure of the response from the calendar api.
     /// </summary>
-    private record Calendar(
+    private record class Calendar(
         CalendarItem[] Races
     );
 
     /// <summary>
     /// The structure of a calendar item of the calendar api.
     /// </summary>
-    private record CalendarItem(
+    private record class CalendarItem
+    (
+        /// <summary>
+        /// The name of the event.
+        /// </summary>
         string Name,
+        /// <summary>
+        /// The session data of the event.
+        /// </summary>
         Dictionary<string, DateTime> Sessions
     );
 }
