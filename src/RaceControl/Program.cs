36f1285fa54099297487398e9977c251d31fbf1c
Traxx186 <justinvanderkruit@icloud.com>
2024-10-27 15:47:53 +0100

    chore: refactor progam.cs

--- src/RaceControl/Program.cs
+++ src/RaceControl/Program.cs
@@ -16,7 +16,7 @@ app.UseForwardedHeaders();
 app.UseWebSockets();
 
 var trackStatus = app.Services.GetRequiredService<TrackStatus>();
-trackStatus.OnTrackFlagChange += flagData => Broadcast(flagData).Wait();
+trackStatus.OnTrackFlagChange += flagData => Broadcast(flagData, CancellationToken.None).Wait();
 
 app.Map("/", async context =>
 {
@@ -31,11 +31,11 @@ app.Map("/", async context =>
     Connections.Add(webSocket);
 
     Log.Information("[Race Control] New user connected, sending current active flag");
-    await SendFlag(webSocket, trackStatus.ActiveFlag);
+    await SendFlag(webSocket, trackStatus.ActiveFlag, context.RequestAborted);
 
-    while (!CancellationToken.IsCancellationRequested && webSocket.State == WebSocketState.Open)
-    { 
-        var result = await webSocket.ReceiveAsync(buffer, CancellationToken);
+    while (!context.RequestAborted.IsCancellationRequested && webSocket.State == WebSocketState.Open)
+    {
+        var result = await webSocket.ReceiveAsync(buffer, context.RequestAborted);
         if (result.MessageType != WebSocketMessageType.Close)
             continue;
 
@@ -47,75 +47,7 @@ app.Map("/", async context =>
 Log.Information("[Race Control] Starting Application");
 await app.RunAsync();
 
-static WebApplication SetupWebApplication(string[] args)
-{
-    var builder = WebApplication.CreateSlimBuilder(args);
-    builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
-    builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
-    builder.Configuration.AddEnvironmentVariables();
-    
-    Log.Logger = new LoggerConfiguration()
-        .ReadFrom.Configuration(
-            builder.Configuration,
-            new ConfigurationReaderOptions(ConfigurationAssemblySource.AlwaysScanDllFiles)
-        )
-        .CreateLogger();
-    
-    builder.Services.AddSerilog();
-    builder.Services.AddSingleton<TrackStatus>();
-    builder.Services.AddSingleton<CategoryService>();
-    
-    // Create DB Context pool.
-    builder.Services.AddDbContextPool<RaceControlContext>(options =>
-        options
-            .UseNpgsql(builder.Configuration["DATABASE_URL"])
-            .UseSnakeCaseNamingConvention()
-    );
-
-    // Add Quartz to services
-    builder.Services.AddQuartz(quartz => 
-    {
-        var syncJobKey = new JobKey("SyncSessionsJob");
-        quartz.AddJob<SyncSessionsJob>(opts => opts.WithIdentity(syncJobKey));
-        quartz.AddTrigger(opts => opts
-            .ForJob(syncJobKey)
-            .WithIdentity("SyncSessionsJob-trigger")
-            .WithSchedule(CronScheduleBuilder.WeeklyOnDayAndHourAndMinute(DayOfWeek.Thursday, 8, 0))
-        );
-        
-        var fetchSessionJobKey = new JobKey("FetchActiveSessionJob");
-        quartz.AddJob<FetchActiveSessionJob>(opts => opts.WithIdentity(fetchSessionJobKey));
-        quartz.AddTrigger(opts => opts
-            .ForJob(fetchSessionJobKey)
-            .WithIdentity("FetchActiveSessionJob-trigger")
-            .WithCronSchedule("0 * * ? * SUN,THU,FRI,SAT")
-        );
-    });
-
-    builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
-    
-    return builder.Build();
-}
-
-// Sends a message to all connected clients.
-static async Task Broadcast(FlagData flagData)
-{
-    Log.Information($"[Race Control] Sending flag '{flagData.Flag}' to all connected clients");
-    var openSockets = Connections.Where(x => x.State == WebSocketState.Open);
-    foreach (var websocket in openSockets)
-        await SendFlag(websocket, flagData);
-}
-
-// Serialize the flag data to json and send the json to the client.
-static async Task SendFlag(WebSocket websocket, FlagData flagData)
-{
-    var json = JsonSerializer.Serialize(flagData);
-    var data = Encoding.UTF8.GetBytes(json);
-
-    await websocket.SendAsync(data, WebSocketMessageType.Text, true, CancellationToken);
-}
-
-public partial class Program 
+public partial class Program
 {
     /// <summary>
     /// All the connected web socket clients.
@@ -123,20 +55,84 @@ public partial class Program
     private static readonly List<WebSocket> Connections = [];
 
     /// <summary>
-    /// Cancellation token for graceful shutdown.
+    /// Create the main web application with the required configuration and linked services
     /// </summary>
-    private static CancellationToken CancellationToken { get; } = SetupGracefulShutdown();
-    
-    /// <summary>
-    /// Creates a new <see cref="CancellationToken"/> object for a graceful shutdown.
-    /// </summary>
-    /// <returns>A <see cref="CancellationToken"/> object.</returns>
-    private static CancellationToken SetupGracefulShutdown()
+    /// <param name="args">Application arguments.</param>
+    /// <returns>The web application.</returns>
+    private static WebApplication SetupWebApplication(string[] args)
     {
-        var tokenSource = new CancellationTokenSource();
-        Console.CancelKeyPress += (_, _) => tokenSource.Cancel();
-        AppDomain.CurrentDomain.ProcessExit += (_, _) => tokenSource.Cancel();
+        var builder = WebApplication.CreateSlimBuilder(args);
+        builder.Configuration.SetBasePath(Directory.GetCurrentDirectory());
+        builder.Configuration.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        builder.Configuration.AddEnvironmentVariables();
 
-        return tokenSource.Token;
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(
+                builder.Configuration,
+                new ConfigurationReaderOptions(ConfigurationAssemblySource.AlwaysScanDllFiles)
+            )
+            .CreateLogger();
+
+        builder.Services.AddSerilog();
+        builder.Services.AddSingleton<TrackStatus>();
+        builder.Services.AddSingleton<CategoryService>();
+
+        // Create DB Context pool.
+        builder.Services.AddDbContextPool<RaceControlContext>(opts => opts
+            .UseNpgsql(builder.Configuration["DATABASE_URL"])
+            .UseSnakeCaseNamingConvention()
+        );
+
+        // Add Quartz to services
+        builder.Services.AddQuartz(quartz =>
+        {
+            var syncJobKey = new JobKey("SyncSessionsJob");
+            quartz.AddJob<SyncSessionsJob>(opts => opts.WithIdentity(syncJobKey));
+            quartz.AddTrigger(opts => opts
+                    .ForJob(syncJobKey)
+                    .WithIdentity("SyncSessionsJob-trigger")
+                    .WithCronSchedule("0 0 8 ? * THU")
+                //.WithSchedule(CronScheduleBuilder.WeeklyOnDayAndHourAndMinute(DayOfWeek.Thursday, 8, 0))
+            );
+
+            var fetchSessionJobKey = new JobKey("FetchActiveSessionJob");
+            quartz.AddJob<FetchActiveSessionJob>(opts => opts.WithIdentity(fetchSessionJobKey));
+            quartz.AddTrigger(opts => opts
+                .ForJob(fetchSessionJobKey)
+                .WithIdentity("FetchActiveSessionJob-trigger")
+                .WithCronSchedule("0 * * ? * SUN,THU,FRI,SAT")
+            );
+        });
+
+        builder.Services.AddQuartzHostedService(q => q.WaitForJobsToComplete = true);
+
+        return builder.Build();
+    }
+
+    /// <summary>
+    /// Sends a websocket response to all the connected client.
+    /// </summary>
+    /// <param name="flagData">Flag data to send.</param>
+    /// <param name="token">If the response needs to be cancelled.</param>
+    private static async Task Broadcast(FlagData flagData, CancellationToken token)
+    {
+        Log.Information($"[Race Control] Sending flag '{flagData.Flag}' to all connected clients");
+        var openSockets = Connections.Where(x => x.State == WebSocketState.Open);
+        foreach (var websocket in openSockets)
+            await SendFlag(websocket, flagData, token);
+    }
+
+    /// <summary>
+    /// Send a websocket message with flag data
+    /// </summary>
+    /// <param name="websocket">The websocket client.</param>
+    /// <param name="flagData">Flag data to send.</param>
+    /// <param name="cancellationToken">If the response needs to be cancelled.</param>
+    private static async Task SendFlag(WebSocket websocket, FlagData flagData, CancellationToken cancellationToken)
+    {
+        var json = JsonSerializer.Serialize(flagData);
+        var data = Encoding.UTF8.GetBytes(json);
+
+        await websocket.SendAsync(data, WebSocketMessageType.Text, true, cancellationToken);
     }
 }
