520c1b1bcf95e24ee844736bbcb7f9fb94e713a2
Traxx186 <justinvanderkruit@protonmail.com>
2025-10-19 16:14:24 +0200

    feat: add latency column

--- src/RaceControl/Database/Entities/Category.cs
+++ src/RaceControl/Database/Entities/Category.cs
@@ -5,5 +5,6 @@ public class Category
     public required string Name { get; init; }
     public short Priority { get; init; }
     public required string Key { get; init; }
+    public required int Latency { get; init; }
     public required ICollection<Session> Sessions { get; init; } = new List<Session>();
 }
---
+++ src/RaceControl/Migrations/20251019141320_AddCategoryLatency.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaceControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryLatency : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "latency",
+                table: "category",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "latency",
+                table: "category");
+        }
+    }
+}
