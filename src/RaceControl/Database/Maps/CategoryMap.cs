c89b4766955054b99849fef2b36811c8ad7358c2
Traxx186 <justinvanderkruit@protonmail.com>
2025-10-26 00:06:17 +0200

    chore: move entity properties to seperate files

---
+++ src/RaceControl/Database/Maps/CategoryMap.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RaceControl.Database.Entities;
+
+namespace RaceControl.Database.Maps;
+
+public class CategoryMap : IEntityTypeConfiguration<Category>
+{
+    public void Configure(EntityTypeBuilder<Category> builder)
+    {
+        builder.HasKey(e => e.Key).HasName("key_pkey");
+        builder.ToTable("category");
+
+        builder.Property(e => e.Key)
+            .HasMaxLength(32)
+            .HasColumnName("key");
+
+        builder.Property(e => e.Name)
+            .HasMaxLength(64)
+            .HasColumnName("name");
+
+        builder.Property(e => e.Priority)
+            .HasColumnName("priority");
+    }
+}
