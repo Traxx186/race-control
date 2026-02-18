f3fc815be825f8da637b478f05370196a33cf96d
Traxx186 <justinvanderkruit@protonmail.com>
2026-02-18 17:38:48 +0100

    feat: add round number to session

---
+++ src/RaceControl/Migrations/20260218161900_AddRoundColumn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RaceControl.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoundColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "round",
+                table: "session",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "round",
+                table: "session");
+        }
+    }
+}
